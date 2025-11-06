dd86a76bc6a509fde3aafb6e3c7501149e8efcb4
itariksahin <85902981+itariksahin@users.noreply.github.com>
2025-11-06 15:20:37 +0300

    06.11.2025
    06.11.2025

--- 06.11.2025/WindowsFormsApp7/WindowsFormsApp7/Form1.cs
+++ 06.11.2025/WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -16,5 +16,47 @@ namespace WindowsFormsApp7
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int bankanot = 0;
+           double hesap=Convert.ToDouble(textBox1.Text)*Convert.ToDouble(textBox2.Text);
+            textBox3.Text = hesap.ToString();
+            while(hesap>=200)
+            {
+                hesap = hesap - 200;
+                bankanot++;
+            }
+            textBox4.Text = bankanot.ToString();
+            bankanot = 0;
+            while (hesap >= 100)
+            {
+                hesap = hesap - 100;
+                bankanot++;
+            }
+            textBox5.Text = bankanot.ToString();
+            bankanot = 0;
+            while (hesap >= 50)
+            {
+                hesap = hesap - 50;
+                bankanot++;
+            }
+            textBox6.Text = bankanot.ToString();
+            bankanot = 0;
+            while (hesap >= 20)
+            {
+                hesap = hesap - 20;
+                bankanot++;
+            }
+            textBox7.Text = bankanot.ToString();
+            bankanot = 0;
+            while (hesap >=10)
+            {
+                hesap = hesap - 10;
+                bankanot++;
+            }
+            textBox8.Text = bankanot.ToString();
+            bankanot = 0;
+        }
     }
 }

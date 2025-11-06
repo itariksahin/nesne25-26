d0905e64c8cfec034f1fe811d40c7b8303ef5da8
itariksahin <85902981+itariksahin@users.noreply.github.com>
2025-11-06 15:27:12 +0300

    11.06.2025
    11.06.2025

--- 06.11.2025/WindowsFormsApp7/WindowsFormsApp7/Form1.cs
+++ 06.11.2025/WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -19,44 +19,53 @@ namespace WindowsFormsApp7
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int bankanot = 0;
-           double hesap=Convert.ToDouble(textBox1.Text)*Convert.ToDouble(textBox2.Text);
-            textBox3.Text = hesap.ToString();
-            while(hesap>=200)
+            try
             {
-                hesap = hesap - 200;
-                bankanot++;
+                int bankanot = 0;
+                double hesap = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text);
+                textBox3.Text = hesap.ToString();
+                while (hesap >= 200)
+                {
+                    hesap = hesap - 200;
+                    bankanot++;
+                }
+                textBox4.Text = bankanot.ToString();
+                bankanot = 0;
+                while (hesap >= 100)
+                {
+                    hesap = hesap - 100;
+                    bankanot++;
+                }
+                textBox5.Text = bankanot.ToString();
+                bankanot = 0;
+                while (hesap >= 50)
+                {
+                    hesap = hesap - 50;
+                    bankanot++;
+                }
+                textBox6.Text = bankanot.ToString();
+                bankanot = 0;
+                while (hesap >= 20)
+                {
+                    hesap = hesap - 20;
+                    bankanot++;
+                }
+                textBox7.Text = bankanot.ToString();
+                bankanot = 0;
+                while (hesap >= 10)
+                {
+                    hesap = hesap - 10;
+                    bankanot++;
+                }
+                textBox8.Text = bankanot.ToString();
+                bankanot = 0;
+                int t_bankonot = Convert.ToInt32(textBox4.Text) + Convert.ToInt32(textBox5.Text) +
+                    Convert.ToInt32(textBox6.Text) + Convert.ToInt32(textBox7.Text) +
+                    Convert.ToInt32(textBox8.Text);
+                textBox9.Text = t_bankonot.ToString();
             }
-            textBox4.Text = bankanot.ToString();
-            bankanot = 0;
-            while (hesap >= 100)
-            {
-                hesap = hesap - 100;
-                bankanot++;
-            }
-            textBox5.Text = bankanot.ToString();
-            bankanot = 0;
-            while (hesap >= 50)
-            {
-                hesap = hesap - 50;
-                bankanot++;
-            }
-            textBox6.Text = bankanot.ToString();
-            bankanot = 0;
-            while (hesap >= 20)
-            {
-                hesap = hesap - 20;
-                bankanot++;
-            }
-            textBox7.Text = bankanot.ToString();
-            bankanot = 0;
-            while (hesap >=10)
-            {
-                hesap = hesap - 10;
-                bankanot++;
-            }
-            textBox8.Text = bankanot.ToString();
-            bankanot = 0;
+            catch { MessageBox.Show("Giriş yapmadınız"); }
+            finally { }
         }
     }
 }

4f3c66db9f405a98febfe3d623155a09613ecdd9
itariksahin <85902981+itariksahin@users.noreply.github.com>
2026-02-19 10:43:14 +0300

    19.02.2026
    19.02.2026

---
+++ 19.02.2026/WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("Form Yükleniyor......");
+            }
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("Form kapatılıyor");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form tıklandı");
+        }
+    }
+}
---
+++ 19.02.2026/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "Miskin hayvan kayıt formu";
+            this.BackColor = Color.DarkRed;
+            this.ForeColor = Color.DarkCyan;
+            Random rnd = new Random();
+            int en = rnd.Next(500, 900);
+            int boy = rnd.Next(700, 1200);
+            this.Size = new Size(en, boy);
+            label1.Text = "Ecrin bunu tıklma";
+            label1.BackColor = Color.Black;
+            label1.ForeColor = Color.White;
+            int x_konumu = rnd.Next(150, 500);
+            int y_konumu = rnd.Next(150, 500);
+            label1.Location = new Point(x_konumu, y_konumu);
+            label1.Size = new Size(x_konumu / 2, y_konumu / 2);
+            textBox1.Location= new Point(x_konumu-50, y_konumu-50);
+            textBox1.Size=new Size(en/10, boy/10);
+            button1.Text = "TIKLA BENİ";
+            button1.BackColor = Color.Purple;
+            button1.ForeColor = Color.ForestGreen;
+            button1.Location=new Point(x_konumu - 90, y_konumu - 90);
+            button1.Size = new Size(en / 10, boy / 20);
+
+        }
+    }
+}
